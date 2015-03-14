6bda3df4ea6a2c3700fdc2d30b0f2c6f6d6ef386
Milkey Mouse <sillystixco@hotmail.com>
2015-03-13 20:36:17 -0700

    Completed Jinja generator for PanelLogic.z

--- CreatePanels/CreatePanels/CreatePanels/CreatePanels.py
+++ CreatePanels/CreatePanels/CreatePanels/CreatePanels.py
@@ -1,10 +1,13 @@
 import glob, os, sys, csv, msvcrt
-from tempita import looper
-ghn = "TheLegendofAdlez"
+import pprint
+from jinja2 import Environment, FileSystemLoader
+ghn = "TheLegendofAdlez_experimental"
 
 levels = glob.glob(os.path.expanduser("~\Documents\GitHub\\" + ghn + "\THE GAME\Content\*.Level.data"))
 
 archetypes = []
+
+print "Parsing levels..."
 
 for levelname in levels:
     with open(levelname, "r") as level:
@@ -24,6 +27,8 @@ for levelname in levels:
 archetypefiles = glob.glob(os.path.expanduser("~\Documents\GitHub\\" + ghn + "\THE GAME\Content\*.Archetype.data"))
 
 zilches = []
+
+print "Parsing archetypes..."
 
 for archetype in archetypefiles:
     scripts = []
@@ -48,6 +53,8 @@ accepted_types = ["Boolean", "Real", "Integer", "String"]
 
 zilch_vars = {}
 
+print "Parsing Zilch scripts..."
+
 for zilch in zilches:
     zname = os.path.expanduser("~\Documents\GitHub\\" + ghn + "\THE GAME\Content\\" + zilch + ".z")
     if not os.path.isfile(zname):
@@ -68,6 +75,8 @@ for zilch in zilches:
 
 arch_vars = {}
 
+print "Formatting tree..."
+
 for archetype in archetypefiles:
     scripts = []
     archname = archetype[archetype.rfind("\\") + 1:archetype.find(".")]
@@ -81,23 +90,87 @@ for archetype in archetypefiles:
                         _avars.append(zilch_vars[fz])
     arch_vars[archname] = _avars
 
-import pprint
-pp = pprint.PrettyPrinter(indent=4)
-ppo = pp.pformat(arch_vars)
-#print ppo
-with open("out.txt", "w") as outfile:
-    outfile.write(ppo)
-#os.startfile("out.txt")
+#pp = pprint.PrettyPrinter(indent=4)
+#ppo = pp.pformat(arch_vars)
+#with open("out.txt", "w") as outfile:
+#    outfile.write(ppo)
+
+print "Creating new Archetypes..."
+
+arch_vars
+
+print "Loading templates..."
+
+env = Environment(loader=FileSystemLoader('templates'))
+tpanel = env.get_template('TemplatePanel.z')
+tpanelarch = env.get_template('TemplatePanel.z')
+
+print "Rebuilding library..."
+
+jinja_arches = []
+
+for arch in arch_vars:
+    _jarch = {}
+    _jarch["name"] = arch
+    _jarch["caps_name"] = arch.capitalize()
+    _jvars = []
+    for script in arch_vars[arch]:
+        for var in script:
+            siq = var[0] + "." + var[1]
+            print "Building " + siq + "..."
+            _jvar = {}
+            _jvar["full"] = siq
+            _jvar["local"] = var[1]
+            if var[2] == "Real":
+                _jvar["real"] = True
+                _jvar["integer"] = False
+                _jvar["bool"] = False
+                _jvar["string"] = False
+            elif var[2] == "Integer":
+                _jvar["real"] = False
+                _jvar["integer"] = True
+                _jvar["bool"] = False
+                _jvar["string"] = False
+            elif var[2] == "String":
+                _jvar["real"] = False
+                _jvar["integer"] = False
+                _jvar["bool"] = False
+                _jvar["string"] = True
+            elif var[2] == "Boolean":
+                _jvar["real"] = False
+                _jvar["integer"] = False
+                _jvar["bool"] = True
+                _jvar["string"] = False
+            _jvars.append(_jvar)
+    _jarch["vars"] = _jvars
+    jinja_arches.append(_jarch)
+
+#pp.pprint(jinja_arches)
+
+print "Building PanelLogic.z..."
+
+with open(os.path.expanduser("~\Documents\GitHub\\" + ghn + "\THE GAME\PanelLogic.z"), "w") as zilchscript: #Content
+    panel_result = tpanel.render(archetypes=jinja_arches)
+    print panel_result
+    zilchscript.write(panel_result)
 
 
-with open('avoid.csv', 'w') as csvfile:
-    avoids = csv.writer(csvfile)
-    for arch in arch_vars:
-        viq = arch_vars[arch]
-        for script in viq:
-            for var in script:
-                siq = var[0] + "." + var[1]
-                print siq + " "
-                ans = msvcrt.getch().upper()
-                if ans == "C":
-                    avoids.writerow((var[0], var[1], var[2]))
+print "Done!"
+
+os.startfile(os.path.expanduser("~\Documents\GitHub\\" + ghn + "\THE GAME\PanelLogic.z"))
+
+if False:
+    answers = {}
+    with open('avoid.csv', 'a') as csvfile:
+        avoids = csv.writer(csvfile)
+        for arch in arch_vars:
+            viq = arch_vars[arch]
+            for script in viq:
+                for var in script:
+                    siq = var[0] + "." + var[1]
+                    print siq + " "
+                    if not siq in answers:
+                        ans = msvcrt.getch().upper()
+                        answers[siq] = ans
+                    if ans == "C":
+                        avoids.writerow((var[0], var[1], var[2]))
