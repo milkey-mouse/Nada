2d37839ee718d9ced8a30aa73e26866dcf28f771
Milkey Mouse <sillystixco@hotmail.com>
2015-04-07 15:55:57 -0700

    Credits final
    Done!

--- Credits/Credits/Credits/Credits.py
+++ Credits/Credits/Credits/Credits.py
@@ -1,6 +1,7 @@
 from jinja2 import Environment, FileSystemLoader
 from collections import defaultdict
-import os, sys
+import glob
+import os
 
 credits = ""
 
@@ -22,6 +23,9 @@ print "Assembling hierarchy..."
 
 linetree = defaultdict(lambda:[]) #constructs a dict full of lists so .append() will never fail
 
+#this messy code below takes each line and scrolls up until it finds a parent (one with a colon at the end and with a lower number of tabs)
+#and sorts then into lists in a dict, with the multi-level ones being handled by linking.
+
 for idx in range(0, len(lines) - 1):
     line = lines[idx]
     sidx = idx
@@ -33,10 +37,40 @@ for idx in range(0, len(lines) - 1):
     if sidx == 0:
         linetree[""].append(lines[idx][0])
 
-linedict = {}
+print "Grabbing images..."
 
-for i in linetree:
-    linedict[i] = linetree[i]
+linedict = defaultdict(lambda:[])
+
+images = {}
+
+for parent in linetree:
+    for item in linetree[parent]:
+        if "~" in item:
+            name = item.split("~")[0]
+            image = item.split("~")[1]
+            if image.endswith(":"):
+                name += ":"
+                image = image[:-1]
+            uid = ""
+            try:
+                with open(glob.glob(os.path.expanduser("~\\Documents\\GitHub\\TheLegendofAdlez\\THE GAME\\Content\\" + image + ".*.meta"))[0], "r") as metafile:
+                    meta = metafile.read()
+                    metas = meta.split("\n")
+                    for levelln in metas:
+                        cln = levelln.replace("\t", "")
+                        if cln.startswith("string Name = "):
+                            uid = cln.replace("\"", "").replace("string Name = ", "").replace(",", "")
+                        if cln.startswith("uint64 ResourceId = "):
+                            uid = cln.replace("uint64 ResourceId = ", "").replace(",", "") + ":" + uid
+                            break
+            except:
+                uid = name
+            images[name] = uid
+            linedict[parent].append(name)
+        else:
+            linedict[parent].append(item)
+
+print images
 
 
 print "Formatting text..."
@@ -78,20 +112,18 @@ for tag in linedict[""]:
 
 print "Compiling level..."
 
-for tag in formatted:
-    print tag
-
-#sys.exit(0)
-
 env = Environment(loader=FileSystemLoader('templates'))
 level_template = env.get_template('CreditsLevelTemplate.Level.data')
 
 jinjadict = []
 
+imgdict = []
+
 uid = 6
 
 pos = 0.0
 
+sign = ""
 
 for item in formatted:
     if item[0] == "54136df68d049509:Triforce":
@@ -99,6 +131,14 @@ for item in formatted:
     elif item[0] == "53f62bf3a4deae78:wendy":
         pos -= 0.5
     jinjadict.append({"font":item[0],"size":item[1],"red":item[2][0],"green":item[2][1],"blue":item[2][2],"text":item[3].replace("\"", "'"),"uid":str(uid),"pos":str(pos)})
+    if item[3] in images:
+        print item[3]
+        if sign == "":
+            sign = "-"
+        else:
+            sign = ""
+        uid += 1
+        imgdict.append({"side":sign,"pos":str(pos),"uid":str(uid),"id":images[item[3]]})
     if item[0] == "54136df68d049509:Triforce":
         pos -= 2
     elif item[0] == "4e7bac697bec55c5:Verdana":
@@ -112,8 +152,10 @@ for item in formatted:
 
 print jinjadict
 
+print imgdict
+
 with open(os.path.expanduser("~\Documents\GitHub\TheLegendofAdlez\THE GAME\Content\Credits.Level.data"), "w") as levelfile:
-    level_result = level_template.render(nodes=jinjadict,last=jinjadict[-1]["pos"])
+    level_result = level_template.render(nodes=jinjadict,last=jinjadict[-1]["pos"],images=imgdict)
     print level_result
     levelfile.write(level_result)
     levelfile.close()
